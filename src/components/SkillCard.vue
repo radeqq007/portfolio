c1aa47fd0e3d920cc8393d12f9d27173e790734f
radeqq007 <radeqq007@interia.pl>
2024-09-12 18:58:25 +0200

    Add skill card hover effect

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="card">
+  <div class="card" ref="card" :style="{ transform: cardTransform }">
     <img :src="img" alt="skillCard" />
     <p>{{ text }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
+import { computed, defineProps, ref } from 'vue';
 
 const props = defineProps({
   img: String,
   text: String,
+});
+
+const card = ref(null);
+const { elementX, elementY, isOutside, elementHeight, elementWidth } =
+  useMouseInElement(card);
+
+const cardTransform = computed(() => {
+  const maxRotation: number = 10;
+
+  const rotationX = (
+    maxRotation / 2 -
+    (elementY.value / elementHeight.value) * maxRotation
+  ).toFixed(2);
+
+  const rotationY = (
+    (elementX.value / elementWidth.value) * maxRotation -
+    maxRotation / 2
+  ).toFixed(2);
+
+  return isOutside.value
+    ? ''
+    : `perspective(${elementWidth.value}px) rotateX(${rotationX}deg)  rotateY(${rotationY}deg)`;
 });
 </script>
 
@@ -26,12 +49,14 @@ const props = defineProps({
 
   filter: grayscale(1);
 
-  transition: 0.1s ease-in-out filter, 0.1s ease-in-out color;
+  transition: 0.2s ease-in-out filter, 0.1s ease-in-out color,
+    0.2s ease-in-out scale;
 }
 
 .card:hover {
   filter: grayscale(0);
   color: var(--text);
+  scale: 1.1;
 }
 img {
   height: 4rem;
