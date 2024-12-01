29ee719ed083276ec139d769d732315a2321e153
radeqq007 <radeqq007@interia.pl>
2024-12-01 13:21:18 +0100

    style: add repo link hover effect

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -81,6 +81,23 @@ p {
 a {
   color: inherit;
   text-decoration: none;
+  position: relative;
+}
+
+a::before {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: var(--primary);
+  transform: scaleX(0);
+  transition: transform 0.1s ease-in-out;
+}
+
+a:hover::before {
+  transform: scaleX(1);
 }
 
 @media screen and (max-width: 1024px) {
