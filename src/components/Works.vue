58ca0e4caa0a9d1fda3934ee4fd6306574b3dd06
radeqq007 <radeqq007@interia.pl>
2024-12-15 11:22:01 +0100

    chore: change imgs format to webp

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -5,7 +5,7 @@
       <ProjectCard
         title="Image to ascii"
         desc="Allows you to select file and convert it to ascii art. This way I learned to work with Pillow python library."
-        imgSrc="images/img-to-ascii.png"
+        imgSrc="images/img-to-ascii.webp"
         :stack="[
           'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg',
         ]"
@@ -14,7 +14,7 @@
       <ProjectCard
         title="This portfolio!"
         desc="This portfolio is more of a playground for me. I packed everything with animations while learning GSAP and vue.js."
-        imgSrc="images/portfolio.png"
+        imgSrc="images/portfolio.webp"
         :stack="[
           'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg',
           'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
@@ -25,7 +25,7 @@
       <ProjectCard
         title="Github stats"
         desc="Get your github stats: most used languages, followers, most starred repos etc."
-        imgSrc="images/github-stats.png"
+        imgSrc="images/github-stats.webp"
         :stack="[
           'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg',
           'svgs/chartjs.svg',
