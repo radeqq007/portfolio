82039fb0e3de09c7839a051dde6244f20052a580
radeqq007 <radeqq007@interia.pl>
2025-02-09 12:02:14 +0100

    feat: nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="bg-secondary w-screen h-screen">
     <router-view />
+    <NavBar />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="w-screen flex justify-center absolute bottom-4 gap-4">
+    <router-link
+      to="/about"
+      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      >ABOUT</router-link
+    >
+    <router-link
+      to="/skills"
+      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      >SKILLS</router-link
+    >
+    <router-link
+      to="/works"
+      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      >WORKS</router-link
+    >
+    <router-link
+      to="/contact"
+      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      >CONTACT</router-link
+    >
+  </nav>
+</template>
+
+<script setup></script>
