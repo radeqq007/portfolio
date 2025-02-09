11c0a21a43a9d2f603ce24c9976a8b448425d1b7
radeqq007 <radeqq007@interia.pl>
2025-02-09 20:01:39 +0100

    feat: setup router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [tailwindcss(), VueRouter({
+    routesFolder: {
+      src: 'src/pages',
+      path: '',
+      filePatterns: (filePatterns) => filePatterns,
+    },
+    filePatterns: ['**/*'],
+    extensions: ["vue"],
+    dts: "typed-router.d.ts",
+    
+  }), vue()],
   base: '/portfolio/',
 });
