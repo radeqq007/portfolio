0f3e777433d7f44e6e037641f3039c56928ad82e
radeqq007 <radeqq007@interia.pl>
2025-02-10 17:37:23 +0100

    feat: add transition to switching routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <main class="bg-secondary w-screen h-screen">
     <Header />
-    <router-view />
+    <transition
+      name="fade"
+      enter-active-class="transition duration-200"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition ease-in duration-200"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <router-view />
+    </transition>
     <NavBar />
   </main>
 </template>
