e0c084649c3d0f37a7d7820348ac04be0d80ed1a
radeqq007 <radeqq007@interia.pl>
2025-02-10 10:10:17 +0100

    feat: add home page

---
+++ src/pages/portfolio/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex justify-center flex-col gap-20">
+    <h1 class="text-9xl font-frederica text-center">Radosław <br/> Kaczmarczyk</h1>
+    <img src="../../assets/scribble.svg" alt="scribble" class="w-screen">
+  </div>
+</template>
