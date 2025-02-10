7c8c5deb64c50102d31bc0a391c1bca47b7ace79
radeqq007 <radeqq007@interia.pl>
2025-02-10 14:24:49 +0100

    style: apply crimson text font to nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,22 +2,22 @@
   <nav class="w-screen flex justify-center absolute bottom-4 gap-4">
     <router-link
       to="/portfolio/about"
-      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      class="text-2xl font-crimson border-1 border-primary rounded-sm px-6 py-1"
       >ABOUT</router-link
     >
     <router-link
       to="/portfolio/skills"
-      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      class="text-2xl font-crimson border-1 border-primary rounded-sm px-6 py-1"
       >SKILLS</router-link
     >
     <router-link
       to="/portfolio/works"
-      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      class="text-2xl font-crimson border-1 border-primary rounded-sm px-6 py-1"
       >WORKS</router-link
     >
     <router-link
       to="/portfolio/contact"
-      class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
+      class="text-2xl font-crimson border-1 border-primary rounded-sm px-6 py-1"
       >CONTACT</router-link
     >
   </nav>
