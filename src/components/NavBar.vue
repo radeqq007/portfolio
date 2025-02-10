6a74669c9fd650bc204b27a4554c18bb6fdf4e57
radeqq007 <radeqq007@interia.pl>
2025-02-10 14:10:28 +0100

    fix: router links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,22 @@
 <template>
   <nav class="w-screen flex justify-center absolute bottom-4 gap-4">
     <router-link
-      to="/about"
+      to="/portfolio/about"
       class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
       >ABOUT</router-link
     >
     <router-link
-      to="/skills"
+      to="/portfolio/skills"
       class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
       >SKILLS</router-link
     >
     <router-link
-      to="/works"
+      to="/portfolio/works"
       class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
       >WORKS</router-link
     >
     <router-link
-      to="/contact"
+      to="/portfolio/contact"
       class="text-2xl border-1 border-primary rounded-sm px-6 py-1"
       >CONTACT</router-link
     >
