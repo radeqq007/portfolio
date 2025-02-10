c42cb18f4afc0c19fb090e12a692d611386f1839
radeqq007 <radeqq007@interia.pl>
2025-02-10 09:58:43 +0100

    style: header layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-screen py-2 px-5">
+  <header class="w-screen py-2 px-5 flex justify-start">
     <router-link to="/portfolio/">
       <h1 class="font-frederica text-4xl">R.K</h1>
     </router-link>
