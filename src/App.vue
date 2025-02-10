4439a12a7e9922653ec0a83ba66e9e677509d510
radeqq007 <radeqq007@interia.pl>
2025-02-10 09:56:17 +0100

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <main class="bg-secondary w-screen h-screen">
+    <Header />
     <router-view />
     <NavBar />
   </main>
 </template>
 
 <script setup lang="ts">
+import Header from './components/Header.vue';
 import NavBar from './components/NavBar.vue';
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="w-screen py-2 px-5">
+    <router-link to="/portfolio/">
+      <h1 class="font-frederica text-4xl">R.K</h1>
+    </router-link>
+  </header>
+</template>
