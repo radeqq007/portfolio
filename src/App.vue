0717cd5e373db5b1d9f38a0343120d4c8990f41a
radeqq007 <radeqq007@interia.pl>
2025-03-16 16:22:40 +0100

    feat: particles background

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,79 @@
     <Works />
     <Contact />
     <Footer />
+
+    <div class="bg">
+      <vue-particles
+        id="tsparticles"
+        @particles-loaded="particlesLoaded"
+        :options="{
+          background: {
+            color: {
+              value: 'transparent',
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'repulse',
+              },
+              onHover: {
+                enable: true,
+                mode: 'grab',
+              },
+            },
+            modes: {
+              repulse: {
+                distance: 250,
+                duration: 0.6,
+              },
+              grab: {
+                distance: 200,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#9b9b9b',
+              opacity: 0.2,
+            },
+            links: {
+              color: '#9b9b9b',
+              distance: 150,
+              enable: true,
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: 'out',
+              random: true,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 120,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 2 },
+            },
+          },
+          detectRetina: true,
+        }"
+      />
+    </div>
   </div>
 </template>
 
@@ -21,6 +94,7 @@ import Nav from './components/Nav.vue';
 import Skills from './components/Skills.vue';
 import Works from './components/Works.vue';
 import { useMouseFollow } from './composables/useMouseFollow';
+import Particles from '@tsparticles/vue3';
 
 // h2 hover effect
 onMounted(() => {
@@ -42,6 +116,16 @@ onMounted(() => {
 </script>
 
 <style>
+.bg {
+  z-index: -1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  pointer-events: none;
+}
+
 h2 {
   font-weight: 400;
   margin-bottom: 0;
@@ -69,5 +153,9 @@ p {
     font-size: 3.4rem;
     text-align: center;
   }
+}
+
+.bg {
+  z-index: -1000;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Particles from '@tsparticles/vue3';
+import { loadFull } from 'tsparticles';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Particles, {
+    init: async engine => {
+      await loadFull(engine);
+    },
+  })
+  .mount('#app');
