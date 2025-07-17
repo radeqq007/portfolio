84a4db9e82d4124530746a70bd8293c1bb600d4b
radeqq007 <radeqq007@interia.pl>
2025-07-17 15:21:31 +0200

    feat: setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
 
@@ -6,6 +7,11 @@ function App() {
     <div className="w-screen h-screen bg-bg bg-[radial-gradient(#413D56_3px,transparent_0)] bg-[size:2rem_2rem] grid place-items-center">
       <main className="w-2/3 h-3/4 border-3 bg-bg/20 border-primary rounded-3xl backdrop-blur-xs">
         <Header />
+        <Router>
+          <Routes>
+            <Route path="/" element={<About />} />
+          </Routes>
+        </Router>
         <Nav />
       </main>
     </div>
