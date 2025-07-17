ae4f4a55281b2550f217b1a32c58cf101d0ed25a
radeqq007 <radeqq007@interia.pl>
2025-07-17 14:42:38 +0200

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="w-screen h-screen bg-bg bg-[radial-gradient(#413D56_3px,transparent_0)] bg-[size:2rem_2rem] grid place-items-center">
+      <main className="w-2/3 h-3/4 border-3 bg-bg/20 border-primary rounded-3xl backdrop-blur-xs"></main>
+    </div>
+  );
 }
 
 export default App;
