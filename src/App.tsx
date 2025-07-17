7eb8ff7718467e00ffbcecb16816dd03ea2bb485
radeqq007 <radeqq007@interia.pl>
2025-07-17 15:42:13 +0200

    feat: add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="w-screen h-screen bg-bg bg-[radial-gradient(#413D56_3px,transparent_0)] bg-[size:2rem_2rem] grid place-items-center">
       <main className="w-2/3 h-3/4 border-3 bg-bg/20 border-primary rounded-3xl backdrop-blur-xs">
         <Header />
-        <Router>
-          <Routes>
-            <Route path="/" element={<About />} />
-          </Routes>
-        </Router>
+        <div className="px-7 py-5">
+          <Router>
+            <Routes>
+              <Route path="/portfolio/" element={<About />} />
+            </Routes>
+          </Router>
+        </div>
         <Nav />
       </main>
     </div>
