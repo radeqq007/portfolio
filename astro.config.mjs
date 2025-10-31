48dd1b3c36e1e7d40f28866f25abe582597f682c
radeqq007 <radeqq007@interia.pl>
2025-10-31 19:49:35 +0100

    chore: format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://radeqq007.github.io',
-  base: '/portfolio',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: "https://radeqq007.github.io",
+	base: "/portfolio",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
