80249733ff58dfb0ba11ba558fa0954b0e6fc6dc
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-07-09 21:49:19 +0200

    fix: scroll to top of the page when changing pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router';
 import Cursor from '@/components/Cursor';
 import Home from '@/pages/Home';
 import Projects from '@/pages/Projects';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="bg-bg min-h-screen min-w-screen md:h-screen">
+      <ScrollToTop />
       <Cursor />
       <Routes>
         <Route path="/" element={<Home />} />  
