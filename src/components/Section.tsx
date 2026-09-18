ecfaed439545e272d8cce74bc93899e37ddd2126
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2026-09-18 11:47:11 +0200

    style: change the background of sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -11,7 +11,7 @@ const Section = ({ title, children, className }: Props) => {
 		<section
 			data-cursor="expand"
 			className={cn(
-				"group border-1 border-accent rounded-xl bg-bg p-4 transition-all duration-300 linear hover:scale-102",
+				"group border-1 border-accent rounded-xl backdrop-blur-xs p-4 transition-all duration-300 linear hover:scale-102",
 				className,
 			)}
 		>
