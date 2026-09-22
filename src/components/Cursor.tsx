570c4bb4d817d118623cff81991d71e91a24e4df
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2026-09-22 14:44:09 +0200

    feat: add mousedown and mouseup animation to cursor

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,8 +1,12 @@
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
 
+const PRESSED_DOWN_SCALE_FACTOR = 0.8
+
 const Cursor = () => {
 	const cursor = useRef<HTMLDivElement>(null);
+	const curScale = useRef<number>(1);
+	const isPressedDown = useRef<boolean>(false);
 
 	useEffect(() => {
 		const cursorFollower = (e: MouseEvent) => {
@@ -18,31 +22,56 @@ const Cursor = () => {
 			const target = e.target as HTMLElement;
 			if (target.closest('[data-cursor="expand"]')) {
 				gsap.to(cursor.current, {
-					scale: 3,
+					scale: isPressedDown.current ? 3 * PRESSED_DOWN_SCALE_FACTOR : 3,
 					duration: 0.4,
 					ease: "back.out",
 				});
+				curScale.current = 3;
 			}
 
 			if (target.closest('[data-cursor="shrink"]')) {
 				gsap.to(cursor.current, {
-					scale: 0.8,
+					scale: isPressedDown.current ? 0.8 * PRESSED_DOWN_SCALE_FACTOR : 0.8,
 					duration: 0.4,
 					ease: "back.out",
 				});
+				curScale.current = 0.8;
 			}
 		};
 
 		const onMouseLeave = (e: MouseEvent) => {
 			const target = e.target as HTMLElement;
-			if (target.closest('[data-cursor="expand"]')) {
+			if (
+				target.closest('[data-cursor="expand"]') ||
+				target.closest('[data-cursor="shrink"]')
+			) {
 				gsap.to(cursor.current, {
-					scale: 1,
+					scale: isPressedDown.current ? PRESSED_DOWN_SCALE_FACTOR : 1,
 					duration: 0.4,
 					ease: "back.out",
 				});
+
+				curScale.current = 1;
 			}
 		};
+
+		const onMouseDown = () => {
+			gsap.to(cursor.current, {
+				scale: curScale.current * PRESSED_DOWN_SCALE_FACTOR,
+				duration: 0.2,
+			})
+			isPressedDown.current = true
+		}
+
+
+		const onMouseUp = () => {
+			gsap.to(cursor.current, {
+				scale: curScale.current,
+				duration: 0.2,
+			})
+			isPressedDown.current = false
+		}
+
 		const mm = gsap.matchMedia();
 
 		mm.add("(pointer: fine)", () => {
@@ -54,6 +83,9 @@ const Cursor = () => {
 			window.addEventListener("mousemove", cursorFollower);
 			window.addEventListener("mouseover", onMouseEnter);
 			window.addEventListener("mouseout", onMouseLeave);
+
+			window.addEventListener("mousedown", onMouseDown);
+			window.addEventListener("mouseup", onMouseUp);
 
 			return () => {
 				window.removeEventListener("mousemove", cursorFollower);
